b3681d5b7af7362d75c1f5cc9fbe5e554ec8e260
Mariusz Dłubak <maniek.14.12.2@gmail.com>
2022-03-30 10:09:56 +0200

    Utworzenie klasy test oraz metody main

---
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab3/test/TestFileRead.java
@@ -0,0 +1,11 @@
+package pl.lublin.wsei.java.cwiczenia.lab3.test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class TestFileRead {
+    public static void main(String[] args) {
+
+    }
+}
