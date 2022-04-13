359382681801456f1fd849fd3d2ee4a064351d7d
Mariusz Dłubak <maniek.14.12.2@gmail.com>
2022-04-13 11:06:04 +0200

    Dodanie listy tytułów wyświetlanej w ListView

--- src/main/java/pl/lublin/wsei/java/cwiczenia/lab3/Controller.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab3/Controller.java
@@ -1,8 +1,11 @@
 package pl.lublin.wsei.java.cwiczenia.lab3;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.stage.FileChooser;
 
 import java.io.File;
@@ -12,6 +15,10 @@ public class Controller {
     FileChooser fileChooser = new FileChooser();
     FileChooser.ExtensionFilter xmlFilter = new FileChooser.ExtensionFilter("Pliki XML (*.xml)", "*.xml");
 
+    public ListView lstInfografiki;
+    ObservableList<String> tytuly = FXCollections.observableArrayList();
+    GusInfoGraphicList igList;
+
     @FXML
     public void initialize() {
         fileChooser.getExtensionFilters().add(xmlFilter);
@@ -20,7 +27,14 @@ public class Controller {
     public void btnOpenFileAction(ActionEvent actionEvent) {
         File file = fileChooser.showOpenDialog(null);
         if (file != null) {
+            igList = new GusInfoGraphicList(file.getAbsolutePath());
             lbFile.setText(file.getAbsolutePath());
+
+            for(Infografika ig: igList.infografiki) {
+                tytuly.add(ig.tytul);
+            }
+
+            lstInfografiki.setItems(tytuly);
         } else {
             lbFile.setText("Proszę wczytać plik ...");
         }
