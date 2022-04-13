e7882cbd5ff61348a89ed8b812ed266a5a9b09c7
Mariusz Dłubak <maniek.14.12.2@gmail.com>
2022-04-13 10:02:52 +0200

    Dodanie GUI do aplikacji oraz obsługi kontrolki typu Label i przycisku

--- src/main/java/pl/lublin/wsei/java/cwiczenia/lab3/Controller.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab3/Controller.java
@@ -1,14 +1,28 @@
 package pl.lublin.wsei.java.cwiczenia.lab3;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 public class Controller {
-    @FXML
-    private Label welcomeText;
+    public Label lbFile;
+    FileChooser fileChooser = new FileChooser();
+    FileChooser.ExtensionFilter xmlFilter = new FileChooser.ExtensionFilter("Pliki XML (*.xml)", "*.xml");
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void initialize() {
+        fileChooser.getExtensionFilters().add(xmlFilter);
+    }
+
+    public void btnOpenFileAction(ActionEvent actionEvent) {
+        File file = fileChooser.showOpenDialog(null);
+        if (file != null) {
+            lbFile.setText(file.getAbsolutePath());
+        } else {
+            lbFile.setText("Proszę wczytać plik ...");
+        }
     }
 }
